2c144a8d25302deb567eee242adcb155bebea135
mphoola <promisemphoola2@gmail.com>
2024-08-02 16:23:16 +0200

    fix: config file returning error

--- src/config/rrlogger.php
+++ src/config/rrlogger.php
@@ -2,5 +2,5 @@
 
 return [
     'retention_days' => env('RRLOGGER_RETENTION_DAYS', 30),
-    'hidden_fields' => explode(',', env('RRLOGGER_HIDDEN_FIELDS', 'password,pin,new_pin')),
+    'hidden_fields' => env('RRLOGGER_HIDDEN_FIELDS', 'password,pin,new_pin'),
 ];
